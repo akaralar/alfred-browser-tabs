73026fe9a82706ac0f56617473370c39f773b736
Ahmet Karalar <akaralar@gmail.com>
2023-05-20 01:57:06 +0200

    Remove Brave, Vivaldi and Chrome, add Arc support

--- src/focus-tab.js
+++ src/focus-tab.js
@@ -3,14 +3,13 @@
 function run(args) {
   ObjC.import("stdlib");
   let browser = $.getenv("browser");
-  let chrome = Application(browser);
-  let query = args[0];
-  let [arg1, arg2] = query.split(",");
-  let windowIndex = parseInt(arg1);
-  let tabIndex = parseInt(arg2);
 
-  chrome.windows[windowIndex].visible = true;
-  chrome.windows[windowIndex].activeTabIndex = tabIndex + 1;
-  chrome.windows[windowIndex].index = 1;
-  chrome.activate();
+  let arc = Application(browser);
+  let query = args[0];
+  let [arg1, arg2, arg3] = query.split(",");
+  let windowIndex = parseInt(arg1);
+  let spaceIndex = parseInt(arg2);
+  let tabIndex = parseInt(arg3);
+
+  arc.windows[windowIndex].spaces[spaceIndex].tabs[tabIndex].select();
 }
--- src/list-tabs.js
+++ src/list-tabs.js
@@ -13,35 +13,45 @@ function run(args) {
     });
   }
 
-  let chrome = Application(browser);
-  chrome.includeStandardAdditions = true;
-  let windowCount = chrome.windows.length;
-  let tabsTitle =
-    browser === "Safari"
-      ? chrome.windows.tabs.name()
-      : chrome.windows.tabs.title();
-  let tabsUrl = chrome.windows.tabs.url();
+  let arc = Application(browser);
+  arc.includeStandardAdditions = true;
+  let windowCount = arc.windows.length;
   let tabsMap = {};
 
   for (let w = 0; w < windowCount; w++) {
-    for (let t = 0; t < tabsTitle[w].length; t++) {
-      let url = tabsUrl[w][t] || "";
-      let matchUrl = url.replace(/(^\w+:|^)\/\//, "");
-      let title = tabsTitle[w][t] || matchUrl;
+    let window = arc.windows[w];
+    for (let s = 0; s < window.spaces.length; s++) {
+      let space = arc.windows[w].spaces[s]
+      let spaceName = space.title();
+      for (let t = 0; t < space.tabs.length; t++) {
+        let tab = space.tabs[t];
+        let url = tab.url() || "";
+        let matchUrl = url.replace(/(^\w+:|^)\/\//, "");
+        let title = tab.title() || matchUrl;
+        let tabID = tab.id();
 
-      tabsMap[url] = {
-        title,
-        url,
-        subtitle: url,
-        windowIndex: w,
-        tabIndex: t,
-        quicklookurl: url,
-        arg: `${w},${t},${url}`,
-        match: `${title} ${decodeURIComponent(matchUrl).replace(
-          /[^\w]/g,
-          " ",
-        )}`,
-      };
+        let subtitle = "";
+        if (tab.location() === "pinned") {
+          subtitle = spaceName + " 📌 | " + url;
+        } else {
+          subtitle = spaceName + " | " + url;
+        }
+
+        tabsMap[tabID] = {
+          title,
+          url,
+          subtitle: subtitle,
+          windowIndex: w,
+          spaceIndex: s,
+          tabIndex: t,
+          quicklookurl: url,
+          arg: `${w},${s},${t},${url}`,
+          match: `${title} ${decodeURIComponent(matchUrl).replace(
+            /[^\w]/g,
+            " ",
+          )}`,
+        };
+      }
     }
   }
 
